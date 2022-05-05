664e4c1f47bd0bd7c8c37ca83c8dcca9a1106df6
Christo Zietsman <christo.zietsman@redstor.com>
2022-05-05 15:42:23 +0200

    Replace access key during build

--- challenger-ng/src/environments/environment.prod.ts
+++ challenger-ng/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { IEnvironment } from '@environments/environment.model';
 export const environment: IEnvironment = {
   production: true,
   unsplash: {
-    accessKey: ''
+    accessKey: 'UNSPLASH_ACCESS_KEY'
   }
 };
