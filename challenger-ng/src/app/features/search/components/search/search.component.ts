6881d0568fa3c04c76275a54209f57b0d85cfda1
PSingh <singh_1992@hotmail.it>
2022-06-02 18:33:14 +0100

    simple search component UI done

--- challenger-ng/src/app/features/search/components/search/search.component.ts
+++ challenger-ng/src/app/features/search/components/search/search.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html'
 })
 export class SearchComponent implements OnInit {
+  searchForm = new FormGroup({
+    search: new FormControl('')
+  });
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  newSearch() {
+    console.log('WORKING', this.searchForm.value);
   }
 
 }
--- challenger-ng/src/app/features/search/search.module.ts
+++ challenger-ng/src/app/features/search/search.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './components/search/search.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 const routes: Routes = [
   {
@@ -16,7 +21,12 @@ const routes: Routes = [
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
   ]
 })
 export class SearchModule { }
