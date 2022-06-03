ee565c6edfefe5ea8f2ee17042969027e7e8dddf
PSingh <singh_1992@hotmail.it>
2022-06-03 12:01:00 +0100

    API key removed

--- challenger-ng/src/environments/environment.ts
+++ challenger-ng/src/environments/environment.ts
@@ -5,6 +5,6 @@ export const environment: IEnvironment = {
   production: false,
   unsplash: {
     // provide add your access key here
-    accessKey: 'O0SZBJMiaWVq--TD6WvEc9BuhbfC3eSyKB3n0AOUSIc'
+    accessKey: ''
   }
 };
