5a11187f7d5281631f65948accf37ae62f1b7a57
musa khosa <39824359+musa-leaf@users.noreply.github.com>
2023-01-09 19:53:35 +0200

    Update environment.ts

--- challenger-ng/src/environments/environment.ts
+++ challenger-ng/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment: IEnvironment = {
   production: false,
   unsplash: {
     // add your access key here and remember to remove it before you create a pull request
-    accessKey: 'Od-jUgcb1WIyQWY4Ml-695WJHE5O78dMBgvJ3jFcdBk',
+    accessKey: 'UNSPLASH_ACCESS_KEY',
     url: 'https://api.unsplash.com'
   }
 };
