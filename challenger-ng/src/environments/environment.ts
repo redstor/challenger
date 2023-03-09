5ca8148284dda042a3f817888547a28358187e5e
Samson Gilbert <onyx@thestartup.home>
2023-03-09 11:18:38 +0200

    Added a 404 page

--- challenger-ng/src/environments/environment.ts
+++ challenger-ng/src/environments/environment.ts
@@ -5,7 +5,7 @@ export const environment: IEnvironment = {
   production: false,
   unsplash: {
     // add your access key here and remember to remove it before you create a pull request
-    accessKey: 'UNSPLASH_ACCESS_KEY',
+    accessKey: 'lIihYg-GjOhCxE105SHURKCOE_poFt9uEd4KWT2Co8g',
     url: 'https://api.unsplash.com'
   }
 };
